3f73881f951d62150fb48e97bfc723b31c804721
GauriShirke12 <152776730+GauriShirke12@users.noreply.github.com>
2024-03-01 11:06:54 +0530

    Add files via upload

---
+++ ArrayAsArgument.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class ArrayAsArgument {
+    public static void update(int marks[], int nonChangable) {
+        nonChangable = 10;
+        for (int i = 0; i < marks.length; i++) {
+            marks[i] = marks[i] + 1;
+        }
+    }
+
+    public static void main(String args[]) {
+        int marks[] = { 98, 97, 99 };
+        int nonChangable = 5;
+        update(marks, nonChangable);
+        System.out.println(nonChangable);
+        // print our marks
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i] + " ");
+        }
+        System.out.println();
+    }
+}
